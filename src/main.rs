673ffffe7e0cc1c1bbb4c6c62e97fc7f9a9c5200
Ryan Goldstein <ryan@ryanisaacg.com>
2020-01-15 16:18:33 -0500

    Add tree support

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,8 @@ fn main() {
 
 async fn app(window: Window, mut gfx: Graphics, mut events: EventStream) -> Result<()> {
     let mut world = World::new();
-    let player_id = world.spawn((Player, Circle::new(Vector::new(0.0, 0.0), 16.0)));
+    let player_id = spawn_player(&mut world);
+    spawn_tree(&mut world, Vector::new(500.0, 200.0));
     let mut mouse_pos = Vector::ZERO;
     loop {
         while let Some(ev) = events.next_event().await {
@@ -45,15 +46,32 @@ async fn app(window: Window, mut gfx: Graphics, mut events: EventStream) -> Resu
         gfx.fill_circle(mouse_pos, 10.0, Color::GREEN);
         let player_bounds = world.get::<Circle>(player_id).unwrap();
         gfx.fill_circle(player_bounds.pos, player_bounds.radius, Color::BLUE);
+        for (_id, (circle, _tree)) in world.query::<(&Circle, &Tree)>().iter() {
+            gfx.fill_circle(circle.pos, circle.radius, Color::RED);
+        }
         gfx.present(&window)?;
     }
 }
 
-struct Player;
+fn spawn_player(world: &mut World) -> Entity {
+    let bounds = Circle::new(Vector::new(0.0, 0.0), 16.0);
+    let light = LightSource { start_angle: 0.0, end_angle: 30.0, radius: 5.0 };
+
+    world.spawn((bounds, light))
+}
+
+fn spawn_tree(world: &mut World, pos: Vector) -> Entity {
+    let bounds = Circle::new(pos, 12.0);
+
+    world.spawn((bounds, AnchorPoint, Tree))
+}
 
 struct LightSource {
-    center: Vector,
     start_angle: f32,
     end_angle: f32,
-    strength: f32
+    radius: f32
 }
+
+struct AnchorPoint;
+
+struct Tree;
