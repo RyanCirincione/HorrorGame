bd010fe70c8b29c9a19c905945616382268e604b
Ryan Goldstein <ryan@ryanisaacg.com>
2020-01-15 15:57:39 -0500

    Add hecs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 // Example 1: The Square
 // Open a window, and draw a colored square in it
+use hecs::*;
 use quicksilver::{
-    geom::{Rectangle, Vector},
+    geom::{Circle, Rectangle, Vector},
     graphics::{Color, Graphics},
     lifecycle::{run, ElementState, Event::*, EventStream, Settings, Window},
     Result,
@@ -19,6 +20,8 @@ fn main() {
 }
 
 async fn app(window: Window, mut gfx: Graphics, mut events: EventStream) -> Result<()> {
+    let mut world = World::new();
+    let player_id = world.spawn((Player, Circle::new(Vector::new(0.0, 0.0), 16.0)));
     let mut mouse_pos = Vector::ZERO;
     loop {
         while let Some(ev) = events.next_event().await {
@@ -34,9 +37,16 @@ async fn app(window: Window, mut gfx: Graphics, mut events: EventStream) -> Resu
             }
         }
         // update
+        for (_id, circle) in world.query::<&mut Circle>().iter() {
+            circle.pos += Vector::ONE;
+        }
         // draw
         gfx.clear(Color::WHITE);
         gfx.fill_circle(mouse_pos, 10.0, Color::GREEN);
+        let player_bounds = world.get::<Circle>(player_id).unwrap();
+        gfx.fill_circle(player_bounds.pos, player_bounds.radius, Color::BLUE);
         gfx.present(&window)?;
     }
 }
+
+struct Player;
